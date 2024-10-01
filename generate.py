4a02b26756a4ed70fc7c925e30dec0a33493cd5d
Derek Chia <derek@derekchia.com>
2024-10-01 22:19:56 +0800

    update

--- generate.py
+++ generate.py
@@ -9,46 +9,33 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # general
-    parser.add_argument(
-        "--ch-version", type=str, default="24.8", help="ClickHouse version"
-    )
+    parser.add_argument("--ch-version", type=str, default="24.8", help="ClickHouse version")
     parser.add_argument("--shard", type=int, default=1, help="Number of shard")
     parser.add_argument("--replica", type=int, default=3, help="Number of replica")
     parser.add_argument("--cluster-directory", type=str, default="cluster_1")
 
     # node configuration
     parser.add_argument("--cpu", type=int, default=1, help="CPUs for each node")
-    parser.add_argument(
-        "--memory", type=str, default="8192m", help="RAM (mb) for each node"
-    )
+    parser.add_argument("--memory", type=str, default="8192m", help="RAM (mb) for each node")
 
     # keeper_mode
-    parser.add_argument(
-        "--keeper-mode",
-        type=str,
-        choices=["chkeeper", "zookeeper", "embedded"],
-        help="chkeeper, zookeeper, embedded",
-    )
-    parser.add_argument(
-        "--keeper-count", type=int, default=3, help="Number of Keepers (Min. 3)"
-    )
+    parser.add_argument("--keeper-mode", type=str, choices=["chkeeper", "zookeeper", "embedded"], help="chkeeper, zookeeper, embedded")
+    parser.add_argument("--keeper-count", type=int, default=3, help="Number of Keepers (Min. 3)")
 
     # keeper node configuration
     parser.add_argument("--cpu-keeper", type=int, default=1, help="CPUs for each node")
-    parser.add_argument(
-        "--memory-keeper", type=str, default="4096m", help="RAM (mb) for each node"
-    )
+    parser.add_argument("--memory-keeper", type=str, default="2048m", help="RAM (mb) for each node")
 
     # ports
     parser.add_argument("--native-protocol-port", type=int, default=9000)
     parser.add_argument("--http-api-port", type=int, default=8123)
-    parser.add_argument("--ch-prometheus-port", type=int, default=9363)
     parser.add_argument("--keeper-raft-port", type=int, default=9234)  # 9444
-    parser.add_argument("--keeper-internal-replication", type=str, default="true")
+    parser.add_argument("--ch-prometheus-port", type=int, default=9363)
 
     # misc.
     parser.add_argument("--chnode-prefix", type=str, default="chnode")
     parser.add_argument("--cluster-name", type=str, default="default")
+    parser.add_argument("--keeper-internal-replication", type=str, default="true")
     parser.add_argument("--jinja-template-directory", type=str, default="default")
     # args.cluster_name = f"cluster_{args.shard}S_{args.replica}R"
 
